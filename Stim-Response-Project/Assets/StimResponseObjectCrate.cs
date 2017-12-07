9fc8f2bb32463b4ed910dd08f60af447f8d55e5f
Elisha Sarkis <elisha.sarkis96@gmail.com>
2017-12-06 22:35:24 -0500

    Create StimResponseObjectCrate and StimResponseObjectFireball Monobehaviour classes, added ignition by "Fire" response to StimResponseObjectCrate

---
+++ Stim-Response-Project/Assets/StimResponseObjectCrate.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class StimResponseObjectCrate : StimResponseObject 
+{
+	[SerializeField] private Color albedoOnFire;
+	private Color initialAlbedo;
+
+	void Awake()
+	{
+		initialAlbedo = GetComponent<MeshRenderer>().material.color;
+	}
+
+	override protected void FireResponse() 
+	{
+		GetComponent<MeshRenderer>().material.color = albedoOnFire;
+	}
+
+	override protected void WaterResponse() 
+	{
+		GetComponent<MeshRenderer>().material.color = initialAlbedo;
+	}
+}
