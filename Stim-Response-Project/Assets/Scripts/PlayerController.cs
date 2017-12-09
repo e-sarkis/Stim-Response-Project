2392ab00fb1c386c65011e783d772af7177f529f
Elisha Sarkis <elisha.sarkis96@gmail.com>
2017-12-08 22:06:14 -0500

    Finish simple 3D MouseLook and Movement script

--- Stim-Response-Project/Assets/Scripts/PlayerController.cs
+++ Stim-Response-Project/Assets/Scripts/PlayerController.cs
@@ -9,15 +9,42 @@ public class PlayerController : MonoBehaviour
 {
 	public float moveSpeed = .15f;
 
+	private float rotX = 0f;
+	private float rotY = 0f;
+
 	void Awake () 
 	{
-
+		Vector3 rot = transform.localRotation.eulerAngles;
+		rotX = rot.x;
+		rotY = rot.y;
 	}
 	
 
 	void Update () 
 	{
+		MouseLook();
 		Move();
+		// Simple Clamping to Floor
+		transform.position = new Vector3(transform.position.x, 1f, transform.position.z);
+	}
+
+
+	public float mouseSensitivity = 50f;
+	public float angleClamp = 75f;
+
+	/// <summary>
+	/// Simple 3D MouseLook
+	/// </summary>
+	void MouseLook()
+	{
+		float mX = Input.GetAxis("Mouse X");
+		float mY = Input.GetAxis("Mouse Y");
+		rotY += mX * mouseSensitivity * Time.deltaTime;
+		rotX += -mY * mouseSensitivity * Time.deltaTime;
+		rotX = Mathf.Clamp(rotX, -angleClamp, angleClamp);
+
+		Quaternion localRot = Quaternion.Euler(rotX, rotY, 0f);
+		transform.rotation = localRot;
 	}
 
 	/// <summary>
@@ -25,20 +52,20 @@ public class PlayerController : MonoBehaviour
 	/// </summary>
 	void Move()
 	{
-		if (Input.GetAxisRaw("Horizontal") > 0)
-		{
-			transform.Translate(transform.forward * moveSpeed); // Move Forward
-		} else if (Input.GetAxisRaw("Horizontal") < 0)
-		{
-			transform.Translate(-transform.forward * moveSpeed); // Move Forward// Move Backward
-		}
-
 		if (Input.GetAxisRaw("Vertical") > 0)
 		{
-			transform.Translate(transform.right * moveSpeed);// Strafe Right
+			transform.Translate(Vector3.forward * moveSpeed); // Move Forward
 		} else if (Input.GetAxisRaw("Vertical") < 0)
 		{
-			transform.Translate(-transform.right * moveSpeed);// Strafe Left
+			transform.Translate(-Vector3.forward * moveSpeed); // Move Forward// Move Backward
+		}
+
+		if (Input.GetAxisRaw("Horizontal") > 0)
+		{
+			transform.Translate(Vector3.right * moveSpeed);// Strafe Right
+		} else if (Input.GetAxisRaw("Horizontal") < 0)
+		{
+			transform.Translate(-Vector3.right * moveSpeed);// Strafe Left
 		}
 	}
 }
