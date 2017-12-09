47e5b1e51df3b1619a6e3ac0e2de279c86f6a03b
Elisha Sarkis <elisha.sarkis96@gmail.com>
2017-12-08 21:29:59 -0500

    Add PlayerController script and place in new directory Scripts

---
+++ Stim-Response-Project/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary>
+/// 3D MouseLook and Transform
+/// </summary>
+public class PlayerController : MonoBehaviour 
+{
+	float moveForce = 1f;
+
+	void Start () 
+	{
+		
+	}
+	
+
+	void Update () 
+	{
+		Move();
+	}
+
+	void Move()
+	{
+		if (Input.GetAxisRaw("Horizontal") > 0)
+		{
+			// Move Forward
+		} else if (Input.GetAxisRaw("Horizontal") < 0)
+		{
+			// Move Backward
+		}
+
+		if (Input.GetAxisRaw("Vertical") > 0)
+		{
+			// Strafe Right
+		} else if (Input.GetAxisRaw("Vertical") < 0)
+		{
+			// Strafe Left
+		}
+	}
+}
