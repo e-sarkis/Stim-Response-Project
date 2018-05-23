61609a688948969a0756f9a5527cb91265834f42
Elisha Sarkis <elisha.sarkis96@gmail.com>
2018-05-22 20:29:52 -0400

    Tidy PlayerController

--- Stim-Response-Project/Assets/Scripts/PlayerController.cs
+++ Stim-Response-Project/Assets/Scripts/PlayerController.cs
@@ -7,7 +7,17 @@ using UnityEngine;
 /// </summary>
 public class PlayerController : MonoBehaviour 
 {
-	public float moveSpeed = .15f;
+	[Header("Movement Settings")]
+	[Range(1.0f, 15.0f)]
+	[Tooltip("Movement speed in Unity units per second")]
+	public float moveSpeed = .05f;
+
+	[Header("Mouselook Settings")]
+	[Range(0.0f, 100.0f)]
+	public float mouseSensitivity 	= 50f;
+	[Range(60.0f, 90.0f)]
+	[Tooltip("Maximum vertical looking angle magnitude (X axis rotation)")]
+	public float angleClamp 		= 75f;
 
 	private float rotX = 0f;
 	private float rotY = 0f;
@@ -23,16 +33,15 @@ public class PlayerController : MonoBehaviour
 	void Update () 
 	{
 		// No MouseLook when attempting object manipulation via "Use" button
-		if (!Input.GetButton("Use")) MouseLook();
+		if (!Input.GetButton("Use"))
+		{
+			MouseLook();
+		}
 
 		Move();
 		// Simple Clamping to Floor
 		transform.position = new Vector3(transform.position.x, 1f, transform.position.z);
 	}
-
-
-	public float mouseSensitivity 	= 50f;
-	public float angleClamp 		= 75f;
 
 	/// <summary>
 	/// Simple 3D MouseLook
@@ -55,20 +64,11 @@ public class PlayerController : MonoBehaviour
 	/// </summary>
 	void Move()
 	{
-		if (Input.GetAxisRaw("Vertical") > 0)
-		{
-			transform.Translate(Vector3.forward * moveSpeed); // Move Forward
-		} else if (Input.GetAxisRaw("Vertical") < 0)
-		{
-			transform.Translate(-Vector3.forward * moveSpeed); // Move Forward// Move Backward
-		}
-
-		if (Input.GetAxisRaw("Horizontal") > 0)
-		{
-			transform.Translate(Vector3.right * moveSpeed);// Strafe Right
-		} else if (Input.GetAxisRaw("Horizontal") < 0)
-		{
-			transform.Translate(-Vector3.right * moveSpeed);// Strafe Left
-		}
+		// Move Forward or Backward
+		transform.Translate(Input.GetAxisRaw("Vertical")	* Vector3.forward
+							* moveSpeed * Time.deltaTime);
+		// Strafe Right
+		transform.Translate(Input.GetAxisRaw("Horizontal")	* Vector3.right
+							* moveSpeed* Time.deltaTime);
 	}
 }
